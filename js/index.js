1ef2da2aa69e42870152cb6453108ec5526b69b1
Sofivnn <msofiane2001@hotmail.fr>
2022-07-15 11:35:12 +0200

    MAJ

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const darkMode = document.getElementById('dark-mode');
+
+darkMode.addEventListener('click', function(){
+    document.body.style="background-color : white";
+    alert("Jennifer est une grognasse")
+})
